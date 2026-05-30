83baffa00a2ec517ab28d7edda1851887a861f89
CD-Portfolio <khodjaevcd@gmail.com>
2026-05-30 23:29:57 +0900

    Update main_window.py

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,9 +1,34 @@
 import tkinter as tk
+
 window = tk.Tk()
 window.geometry("800x600")
 window.title("DTV - Dialogue Editor")
-my_canvas = tk.Canvas(window, width=780, height=580, bg="lightblue")
+
+my_canvas = tk.Canvas(
+    window,
+    width=780,
+    height=580,
+    bg="lightblue"
+)
+
 my_canvas.pack()
-my_canvas.create_rectangle(290, 240, 490, 340, fill="white")
-my_canvas.create_text(390, 290, text="Geralt", fill="black")
+
+center_x = 780 / 2
+center_y = 580 / 2
+
+my_canvas.create_rectangle(
+    center_x - 100,
+    center_y - 50,
+    center_x + 100,
+    center_y + 50,
+    fill="white"
+)
+
+my_canvas.create_text(
+    center_x,
+    center_y,
+    text="Geralt",
+    fill="black"
+)
+
 window.mainloop()
